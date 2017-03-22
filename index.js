6fb79f71f9a8fdbf3afca0d7fca83db60b722eb3
Seth Jennings <sjenning@redhat.com>
2017-03-22 16:40:40 -0500

    lets do this

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var os = require("os");
+
+var hostname = os.hostname();
+var app = express()
+ 
+app.get('/demo', function(req, res) {
+  res.json({
+    message: "Welcome to OpenShift! This message is brought to you by backend pod " + hostname,
+    data: null
+  })
+})
+ 
+app.listen(3000)
