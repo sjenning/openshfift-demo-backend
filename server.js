d0762be794cbcdf70728a88f49272933b24ba80a
Seth Jennings <sjenning@redhat.com>
2017-03-22 17:19:34 -0500

    no caching

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var os = require("os");
 
 var hostname = os.hostname();
 var app = express()
+
+function nocache(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}
  
-app.get('/demo', function(req, res) {
+app.get('/demo', nocache, function(req, res) {
   res.json({
     message: "Welcome to OpenShift! This data is brought to you by backend pod " + hostname,
     data: null
